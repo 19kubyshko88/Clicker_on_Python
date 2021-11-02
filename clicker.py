09a596b729316955454a8d3b60c89381087498fe
Pavel <19kubyshko88@gmail.com>
2021-11-02 16:04:24 +0300

    feat: добавили фон (canvas), окно ввода имени
    Меню с кнопками "Файл", "Справка". С функциями выход,  показать рекорды, помощь и  о программе.
    
    Создали модуль с глобальными переменными для счётчика кликов.

--- clicker.py
+++ clicker.py
@@ -1,7 +1,9 @@
+import global_vars
 from tkinter import *
 from time import time
-import global_vars
+from PIL import ImageTk, Image
 from tkinter import messagebox as mb
+
 
 # import  tkinter as tk
 def show_help():
@@ -17,12 +19,14 @@ def show_about():
     mb.showinfo(message="Сделал РОББО")
 
 def clicker():
-
+    def entry_clear(z):
+        name_entry.delete(0,END)
 
     def check_record(name: str, points: int) -> None:
         with open('clicker_records.txt', 'r+', encoding='utf-8') as f:
+            print(name)
             record_list = f.readlines()
-            new_record_list = [line.strip('\n').split() for line in record_list]
+            new_record_list = [line.strip('\n').split(";") for line in record_list]
 
             for index, line_num in enumerate(new_record_list):
                 if points > int(line_num[2]):
@@ -30,7 +34,8 @@ def clicker():
                     break
             for line_num in range(index + 1, len(new_record_list)):
                 new_record_list[line_num][0] = str(line_num + 1)
-            record_list = [" ".join(i) + "\n" for i in new_record_list]
+            print(record_list)
+            record_list = [";".join(i) + "\n" for i in new_record_list]
             f.seek(0)
             f.writelines(record_list)
 
@@ -42,18 +47,24 @@ def clicker():
             lbl.config(text=f"Кликов\n{global_vars.cc}")
         else:
             confirm =mb.showinfo(message=f'Хватит жать!\nВаш результат {global_vars.cc}!')
+            check_record(name_entry.get(),global_vars.cc)
             if confirm=="ok":
 
                 lbl.config(text=f"Кликов\n0")
                 global_vars.cc=0
 
     root = Tk()
-    root.geometry('400x300+2500+200')
+    root.geometry('400x300+250+200')
     root.resizable(False, False)
     icon_img = PhotoImage(file="hummer.png")
     root.iconposition(100, 50)
     root.iconphoto(False, icon_img)
 
+    bg = ImageTk.PhotoImage(file = 'earth.jpg')
+    btn_bg = ImageTk.PhotoImage(file = 'earth2.jpg')
+    my_canvas = Canvas(root, width = 400, height = 300,  highlightthickness = 0)
+    my_canvas.pack()
+    my_canvas.create_image(-100,-100, image = bg, anchor = 'nw')
     """
     Создаём панель меню под заголовком окна
     """
@@ -83,7 +94,11 @@ def clicker():
                  fg="#96B254",
                  relief=SOLID,
                  bd=10,
-                 command=on_click
+                 command=on_click,
+                 image = btn_bg,
+                 compound = CENTER,
+                 padx = 70,
+                 pady=10
                  )
     lbl = Label(text="Кликов\n0",
                 font=("Comic Sans", 15, 'bold'),
@@ -94,8 +109,16 @@ def clicker():
                 anchor='center',
                 justify=CENTER
                 )
-    lbl.pack()
-    btn.pack()
+    name_entry = Entry(root, width=20)
+    name_entry.insert(0, "username")
+    lbl_window = my_canvas.create_window(200,20, anchor = "n", window = lbl)
+    btn_window = my_canvas.create_window(200,100, anchor = "n", window = btn)
+    entry_window = my_canvas.create_window(200,250, anchor = "n", window = name_entry)
+
+    name_entry.bind("<Button-1>", entry_clear)
+
+    # lbl.pack()
+    # btn.pack()
 
     root.mainloop()
 
